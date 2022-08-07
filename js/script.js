7cc93f755e41d554821aad7f365ab2ae0d8056eb
Bruno <juegos.malafar@gmail.com>
2022-08-07 15:34:30 -0300

    Se continua seccion productos

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 const contenedorProductos = document.querySelector('#contenedorProductos');
 
-function generarCard(producto){
+function generarCard(producto) {
 
-const card = document.createElement("div");
-card.classList.add("col-lg-4", "col-sm-6", "mt-3", "mb-3");
-contenedorProductos.appendChild(card);
+    const card = document.createElement("div");
+    card.classList.add("col-lg-4", "col-sm-6", "mt-3", "mb-3", "cardProducto");
+    contenedorProductos.appendChild(card);
 
-const imagen = document.createElement("img");
-imagen.classList.add("img-fluid");
-imagen.src = "../assets/Agendas/Modelo 1/Agenda modelo Epu 1.webp";
-card.appendChild(imagen);
+    const imagen = document.createElement("img");
+    imagen.classList.add("img-fluid");
+    imagen.src = "../assets/Agendas/Modelo 1/Agenda modelo Epu 1.webp";
+    card.appendChild(imagen);
 }
 
-
-generarCard();
-generarCard();
-generarCard();
-generarCard();
-generarCard();
-generarCard();
-generarCard();
+fetch("../productos.json")
+    .then(res => res.json())
+    .then(productos => {
+        productos.forEach(producto => {
+            generarCard(producto);
+        });
+    });
